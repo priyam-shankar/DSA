eb85594b8c90e2fad3151b10d35a169b133a244e
Priyam Shankar <priyamshankar3333@gmail.com>
2025-03-04 09:23:34 +0530

    updated singleElementInSortedArr.cpp with changes

--- singleElementInSortedArr.cpp
+++ singleElementInSortedArr.cpp
@@ -1,69 +1,43 @@
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-
-// int singleNonDuplicate(vector<int>& A) {
-//     int n = A.size();
-//     if(n == 1) return A[0];
-//     int st = 0, end = n-1;
-    
-//      while (st <= end) {
-//         int mid = st + (end - st) / 2;
-//         if(mid == 0 && A[mid] != A[mid + 1]) return A[mid];
-//         if(mid == n-1 && A[mid] != A[mid - 1]) return A[mid];
-
-//         if(mid % 2 == 0) {  //even no of elements in left or right side of mid 
-//             if(A[mid - 1] == A[mid]) {  //left side jao
-//                 end = mid - 1;
-//             }else { // right side jao
-//                 st = mid + 1; 
-//             }
-//         }else { //odd no of elements in left or right side of mid 
-//             if(A[mid - 1] == A[mid]) {  // right side jao
-//                 st = mid + 1;
-//             }else { //left side jao
-//                 end = mid - 1;
-//             }
-//         }
-//      } 
-
-// }
-// int main() {
-//     vector<int> A = {1,1,2,3,3,4,4,8,8};
-//     cout << singleNonDuplicate(A) <<endl;
-//     return 0;
-// }
-
 #include<iostream>
 #include<vector>
 using namespace std;
 
 int singleNonDuplicate(vector<int>& A) {
     int n = A.size();
-    if(n == 1) return A[0]; // Agar ek hi element hai, toh wahi answer hoga
+    if (n == 1) {
+        return A[0];
+    }
 
     int st = 0, end = n-1;
-    
-    while (st < end) {
+    while (st <= end) {
         int mid = st + (end - st) / 2;
-
-        // Ensure mid is even (so we always check pairs correctly)
-        if (mid % 2 == 1) {
-            mid--;
+        if(mid == 0 && A[0] != A[1]) {  // ye dono mera edge cases
+            return A[mid];
         }
-
-        // If mid and mid+1 are same, single element lies on right side
-        if (A[mid] == A[mid + 1]) {
-            st = mid + 2; // Move right
-        } else {
-            end = mid; // Move left
+        if(mid == n-1 && A[n-1] != A[n-2]) {    //ye bhi edge cases
+            return A[mid];
+        }
+        if(A[mid - 1] != A[mid] && A[mid] != A[mid + 1]) {  // agar starting wla mid hi answer hua toh
+            return A[mid];
+        }
+        if(mid % 2 == 0){   //even no of elemnts in left or right side 
+            if (A[mid] == A[mid - 1]) { // left side jao 
+                end = mid - 1;
+            }else { //right side jao 
+                st = mid + 1;
+            }
+        }else { //odd no of elements in left or right side 
+            if (A[mid] == A[mid - 1]) { //right side jao 
+                st = mid + 1;
+            }else {  //left side jao 
+                end = mid - 1;
+            }
         }
     }
-    return A[st];
+    return -1;
 }
-
-int main() {
+int main (){
     vector<int> A = {1,1,2,3,3,4,4,8,8};
-    cout << singleNonDuplicate(A) << endl;
+    cout <<singleNonDuplicate(A)<< endl;
     return 0;
 }
