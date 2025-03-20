01567abf4b1091e2b0a86c04ec8d95ea3078e1ff
Priyam Shankar <priyamshankar3333@gmail.com>
2025-03-20 23:14:28 +0530

    update changes in bubbleSort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,3 +1,8 @@
+//Concept : for apllying the bubble sort algo on any array :(i). (n-1) iteration ; mtlb We have to apply (n-1)loop 
+//                                                          (ii).  Compare the adjacent elements
+//                                                          (iii). larger elements ko last me push krna h; using swapping 
+// Overall concept : n-1 iteration mtlb mtlb ki 1st iteration se last - 1 iteration tk hm largest elemnts ko last me push krte jaate h.
+// toh 1st iteration me hmara sabse largest element last me push ho jayega or aise hi krte krte n-1 iterartion tk sare largest elements last me push ho jayenge mtlb hmrara array ascending order me sort ho jayega.
 #include<iostream>
 using namespace std;
 void bubbleSort(int arr[], int n) {
