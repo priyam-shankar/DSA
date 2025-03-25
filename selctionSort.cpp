0fba66c0e2fe44f88c60c6a2c64bc8d451760812
Priyam Shankar <priyamshankar3333@gmail.com>
2025-03-26 01:16:30 +0530

    upadte my selection sort code

--- selctionSort.cpp
+++ selctionSort.cpp
@@ -1,55 +1,25 @@
 #include<iostream>
 using namespace std;
 void selectionSort(int arr[], int n) {
-    for(int i=0; i<n-1; i++) {
-        int smallestIdx = i;
-        for(int j=i+1; j<n; j++) {
+    for(int i = 0; i<n-1; i++) {
+        int smallestIdx = i;  // i wale element ko smallest maan liya
+        for(int j = i + 1; j<n; j++) {
             if(arr[j] < arr[smallestIdx]) {
-                arr[smallestIdx] = arr[smallestIdx] - arr[j];
-                arr[j] = arr[smallestIdx] + arr[j];
-                arr[smallestIdx] = arr[j] - arr[smallestIdx];
+                smallestIdx = j;  // Naya smallest element mil gaya
             }
         }
+        swap(arr[i],arr[smallestIdx]);   // Smallest ko i wali position pe fix kar do
     }
 }
 void printArray(int arr[], int n) {
-    for(int i=0; i<n; i++){ // add 
-        cout<< arr[i] <<" ";
+    for(int i = 0; i<n ; i++) {
+        cout <<arr[i] <<" ";
     }
-    cout<< endl;
+    cout << endl;
 }
 int main() {
-    int n = 5;
     int arr[] = {4, 1, 5, 2, 3};
-    selectionSort(arr,n);
-    printArray(arr,n);
-    return 0;
-}
-
-
-
-#include<iostream>
-using namespace std;
-void selectionSort(int arr[], int n) {
-    for(int i=0; i<n-1; i++) {
-        int smallestIdx = i;
-        for(int j=i+1; j<n; j++) {
-            if(arr[j] < arr[smallestIdx]) {
-                smallestIdx = j;
-            }
-        }
-        swap(arr[i], arr[smallestIdx]);
-    }
-}
-void printArray(int arr[], int n) {
-    for(int i=0; i<n; i++){
-        cout<< arr[i] <<" ";
-    }
-    cout<< endl;
-}
-int main() {
-    int n = 5;
-    int arr[] = {4, 1, 5, 2, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
     selectionSort(arr,n);
     printArray(arr,n);
     return 0;
