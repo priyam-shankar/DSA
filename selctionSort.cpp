7143101727590f02ec8b8d6754012e0abe4a9608
Priyam Shankar <priyamshankar3333@gmail.com>
2025-04-01 23:42:46 +0530

    apply changes

--- selctionSort.cpp
+++ selctionSort.cpp
@@ -11,7 +11,7 @@ void selectionSort(int arr[], int n) {
         swap(arr[i],arr[smallestIdx]);   // Smallest ko i wali position pe fix kar do
     }
 }
-void printArray(int arr[], int n) {   
+void printArray(int arr[], int n) {   // function for printing the array 
     for(int i = 0; i<n ; i++) {
         cout <<arr[i] <<" ";
     }
