a8979a45e0173f5b5fc6e6bd165282efb636db81
Priyam Shankar <priyamshankar3333@gmail.com>
2025-04-23 19:12:47 +0530

    update changes

--- random.cpp
+++ random.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+
 int main() {
     int base = 3;
     int exponent = 3;
     int ans = 1;
-    for(int i = 1; i<=n; i++){
+
+    for (int i = 1; i <= exponent; i++) {
         ans *= base;
     }
-    cout<< ans <<endl;
+
+    cout << ans << endl;
     return 0;
 }
