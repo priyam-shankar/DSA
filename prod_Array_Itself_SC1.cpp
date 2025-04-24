9edf4b466f38519c22597e970f9fb228469b987f
Priyam Shankar <priyamshankar3333@gmail.com>
2025-04-25 00:11:41 +0530

    optimized sol

--- prod_Array_Itself_SC1.cpp
+++ prod_Array_Itself_SC1.cpp
@@ -1,20 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
 vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
-    vector<int> ans(n,1);
-    vector<int> prefix(n,1);
-    vector<int> suffix(n,1);
+    vector<int> ans(n, 1);
+    vector<int> prefix(n, 1);
+    vector<int> suffix(n, 1);
 
-    //prefix
-    for(int i = 1; i < n; i++) {
-        prefix[i] = prefix[i-1] * nums[i-1];
+    // Compute prefix products
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
     }
 
-    //suffix
-    for(int i = n-2; i >=0; i-- ) {
-        suffix[i] = suffix[i+1] * nums[i+1];
+    // Compute suffix products
+    for (int i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1];
     }
-     for(int i = 0; i<n; i++) {
+
+    // Multiply prefix and suffix for the final answer
+    for (int i = 0; i < n; i++) {
         ans[i] = prefix[i] * suffix[i];
-     }
-     return ans;
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = productExceptSelf(nums);
+
+    cout << "Product of array except self: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
