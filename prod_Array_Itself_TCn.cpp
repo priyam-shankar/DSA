9c56f1591e5a6af520a3135445ea7341e68cc243
Priyam Shankar <priyamshankar3333@gmail.com>
2025-04-25 00:00:10 +0530

    update

--- prod_Array_Itself_TCn.cpp
+++ prod_Array_Itself_TCn.cpp
@@ -1,13 +1,19 @@
-vector<int> productExceptSelf(vector<int>&nums) {
+vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
     vector<int> ans(n, 1);
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j<n; j++) {
-            if(i != j) {
-                ans[i] *= nums[j];
-            }
-        }
+    // Step 1: Compute prefix products
+    int prefix = 1;
+    for (int i = 0; i < n; ++i) {
+        ans[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    // Step 2: Compute suffix products and multiply with prefix
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; --i) {
+        ans[i] *= suffix;
+        suffix *= nums[i];
     }
 
     return ans;
