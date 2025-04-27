2a9aa132cd8ea099d16e999b2e6f09a42f510388
Priyam Shankar <priyamshankar3333@gmail.com>
2025-04-27 23:31:21 +0530

    update changes

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,33 +1,21 @@
-#include <iostream>
-using namespace std;
-
 void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
+    for(int i=1; i<n; i++){
+        int curr = arr[i];
+        int prev = i-1;
 
-       
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+        while(prev >= && arr[prev] > curr) {
+            arr[prev+1] = arr[prev];
+            prev--;
         }
-        arr[j + 1] = key;
+
+        arr[prev+1] = curr;
     }
 }
-
-void printArray(int arr[], int n) {      //print array elements
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
+int main(){
+    int n = 5;
+    int arr[] = {4,1,5,2,3};
 
     insertionSort(arr, n);
-    cout << "Sorted array: ";
     printArray(arr, n);
-
     return 0;
 }
