abf69967f18c24a4e84ecde1919ba526c8705686
Priyam Shankar <priyamshankar3333@gmail.com>
2025-07-14 19:11:06 +0530

    add

--- decNumToBinary.cpp
+++ decNumToBinary.cpp
@@ -5,19 +5,21 @@ int decToBinary(int decNum) {
     int ans = 0 , pow = 1 ;
     while(decNum > 0) {
          int rem = decNum % 2;
-         decNum = decNum / 2;
          ans = ans + (rem * pow);
+         decNum = decNum / 2;
+         
          pow = pow * 10;
     }
     return ans; 
 }
 int main() {
     int decNum;
-    //cout << "Enter Your Decimal No: " ;
-    //cin >> decNum;
+    cout << "Enter Your Decimal No: " ;
+    cin >> decNum;
+    cout<< decToBinary(decNum);
     
-    for(int i = 0; i<=10; i++) {
-        cout << decToBinary(i) << endl;
-    }
+    // for(int i = 0; i<=10; i++) {
+    //     cout << decToBinary(i) << endl;
+    // }
     return 0;
 }
