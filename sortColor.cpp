c7052f21656dfd1e2d193c5a43ad52ec5d28c1f3
Priyam Shankar <priyamshankar3333@gmail.com>
2025-08-21 07:51:14 +0530

    sorting by existing sort function of 0s, 1s, and 2s

---
+++ sortColor.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void sortColors(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+}
+int main() {
+    vector<int> nums = {2,0,2,1,1,0};
+    sortColors(nums);
+    for(int i = 0; i<nums.size()-1; i++) {
+        cout<<nums[i];
+        if(i != nums.size()-2){
+            cout<< ", ";
+        }
+    }
+    return 0;
+}
