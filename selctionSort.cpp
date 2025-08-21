5a11712b5034430920d915488b9a45fa733797ec
Priyam Shankar <priyamshankar3333@gmail.com>
2025-08-21 07:50:19 +0530

    Selection sort review

--- selctionSort.cpp
+++ selctionSort.cpp
@@ -1,26 +1,26 @@
 #include<iostream>
-using namespace std;
+using namespace std ;
 void selectionSort(int arr[], int n) {
     for(int i = 0; i<n-1; i++) {
-        int smallestIdx = i;  // i wale element ko smallest maan liya
-        for(int j = i + 1; j<n; j++) {
+        int smallestIdx = i;
+        for(int j = i+1; j<n; j++) {
             if(arr[j] < arr[smallestIdx]) {
-                smallestIdx = j;  // Naya smallest element mil gaya
+                smallestIdx = j;
             }
         }
-        swap(arr[i],arr[smallestIdx]);   // Smallest ko i wali position pe fix kar do
+        swap(arr[i],arr[smallestIdx]);
     }
 }
-void printArray(int arr[], int n) {   
-    for(int i = 0; i<n ; i++) {
-        cout <<arr[i] <<" ";
+void printArray(int arr[], int n) {
+    for(int i = 0; i<n; i++) {
+        cout<< arr[i] <<" ";
     }
     cout << endl;
 }
 int main() {
-    int arr[] = {4, 1, 5, 2, 3};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = 5;
+    int arr[] = {4,1,5,2,3};
     selectionSort(arr,n);
-    printArray(arr,n);
+    printArray(arr, n);
     return 0;
 }
